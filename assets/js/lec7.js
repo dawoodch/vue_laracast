87cd10d0c31d90764152b197176f97a34874ee31
Muhammad Dawood <mdawoodch16@gmail.com>
2019-11-02 16:31:49 +0500

    Lec 8: template into template

--- assets/js/lec7.js
+++ assets/js/lec7.js
@@ -1,14 +1,27 @@
-Vue.component('task',{
-    template: '<li><slot></slot></li>',
+
+Vue.component('task-list', {
+    template: `
+            <ul>
+                <task v-for="task in tasks" v-text="task.task"></task>      
+            </ul>
+    `,
     data(){
         return{
-            message: 'Foobar'
+            tasks:[
+                {task:'one', complete: true},
+                {task:'two', complete: true},
+                {task:'three', complete: false},
+                {task:'four', complete: true}
+            ]
         }
     }
+});
+
+Vue.component('task',{
+    template: '<li></li>',
 });
 
 
 new Vue({
     el: '#root',
-
 });
